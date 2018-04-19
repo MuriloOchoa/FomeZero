2d71d7c003a585d7731137d3e97c7e92297660d9
MuriloOchoa <spark909@gmail.com>
2018-04-19 17:26:34 -0300

    Criação da aplicação, das tabelas, relacionamentos e adição de checkboxes para selecionar ingredients ao criar uma dish

---
+++ db/migrate/20180412205046_add_ingredients_to_dish.rb
@@ -0,0 +1,8 @@
+class AddIngredientsToDish < ActiveRecord::Migration[5.1]
+  def change
+    create_table :ingredients_dishes, id: false do |t|
+      t.belongs_to :ingredient, index: true
+      t.belongs_to :dish, index: true
+    end
+  end
+end
